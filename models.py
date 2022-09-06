43a6ea35db0bc6a9d9294e472bb21476fcf8ad91
Geena Jackson <geena.jackson96@gmail.com>
2022-09-06 09:48:27 -0500

    change text input to string input

--- models.py
+++ models.py
@@ -14,8 +14,8 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-    username = db.Column(db.Text(20), primary_key=True, unique=True)
+    username = db.Column(db.String(20), primary_key=True, unique=True)
     password = db.Column(db.Text, nullable=False)
-    email = db.Column(db.Text(50), nullable=False, unique=True)
-    first_name = db.Column(db.Text(30), nullable=False)
-    last_name = db.Column(db.Text(30), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
