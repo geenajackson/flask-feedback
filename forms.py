40e81d86ce95d89517f5324798ebc3c41d847f9d
Geena Jackson <geena.jackson96@gmail.com>
2022-09-08 09:30:58 -0500

    create form for adding feedback

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Email
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import InputRequired, Email, Length
 
 
 class RegisterUserForm(FlaskForm):
@@ -17,3 +17,9 @@ class LoginUserForm(FlaskForm):
 
     username = StringField("Username", validators=[InputRequired()])
     password = PasswordField("Password", validators=[InputRequired()])
+
+class FeedbackForm(FlaskForm):
+    """Form for adding feedback."""
+
+    title = StringField("Title", validators=[InputRequired(), Length(max=100)])
+    content = TextAreaField("Content", validators=[InputRequired()])
